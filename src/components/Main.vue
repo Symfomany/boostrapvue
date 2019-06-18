c8e9b4234e1a532d2bd02293ab06f89d69d086e4
Your Name <you@example.com>
2019-06-18 15:04:31 +0200

    add changes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -88,7 +88,203 @@ export default {
       this.cleared();
     }
   },
+  created() {
+    const datas = [
+      {
+        title: "Old Man's War",
+        author: {
+          firstName: "John",
+          lastName: "Scalzi",
+          note: 5,
+          tel: "0674585649"
+        }
+      },
+      {
+        title: "The Lock Artist",
+        author: {
+          firstName: "Steve",
+          lastName: "Hamilton",
+          note: 2,
+          tel: "0774585612"
+        }
+      },
+      {
+        title: "HTML5",
+        author: {
+          firstName: "Remy",
+          lastName: "Sharp",
+          note: 3,
+          tel: "0174585649"
+        }
+      },
+      {
+        title: "Right Ho Jeeves",
+        author: {
+          firstName: "P.D",
+          lastName: "Woodhouse",
+          note: 1,
+          tel: "0274585649"
+        }
+      },
+      {
+        title: "The Code of the Wooster",
+        author: {
+          firstName: "P.D",
+          lastName: "Woodhouse",
+          note: 4,
+          tel: "0174581642"
+        }
+      },
+      {
+        title: "Thank You Jeeves",
+        author: {
+          firstName: "P.D",
+          lastName: "Woodhouse",
+          note: 3,
+          tel: "0274585443"
+        }
+      },
+      {
+        title: "The DaVinci Code",
+        author: {
+          firstName: "Dan",
+          lastName: "Brown"
+        }
+      },
+      {
+        title: "Angels & Demons",
+        author: {
+          firstName: "Dan",
+          lastName: "Brown"
+        }
+      },
+      {
+        title: "The Silmarillion",
+        author: {
+          firstName: "J.R.R",
+          lastName: "Tolkien"
+        }
+      },
+      {
+        title: "Syrup",
+        author: {
+          firstName: "Max",
+          lastName: "Barry"
+        }
+      },
+      {
+        title: "The Lost Symbol",
+        author: {
+          firstName: "Dan",
+          lastName: "Brown"
+        }
+      },
+      {
+        title: "The Book of Lies",
+        author: {
+          firstName: "Brad",
+          lastName: "Meltzer"
+        }
+      },
+      {
+        title: "Lamb",
+        author: {
+          firstName: "Christopher",
+          lastName: "Moore"
+        }
+      },
+      {
+        title: "Fool",
+        author: {
+          firstName: "Christopher",
+          lastName: "Moore"
+        }
+      },
+      {
+        title: "Incompetence",
+        author: {
+          firstName: "Rob",
+          lastName: "Grant"
+        }
+      },
+      {
+        title: "Fat",
+        author: {
+          firstName: "Rob",
+          lastName: "Grant"
+        }
+      },
+      {
+        title: "Colony",
+        author: {
+          firstName: "Rob",
+          lastName: "Grant"
+        }
+      },
+      {
+        title: "Backwards, Red Dwarf",
+        author: {
+          firstName: "Rob",
+          lastName: "Grant"
+        }
+      },
+      {
+        title: "The Grand Design",
+        author: {
+          firstName: "Stephen",
+          lastName: "Hawking"
+        }
+      },
+      {
+        title: "The Book of Samson",
+        author: {
+          firstName: "David",
+          lastName: "Maine"
+        }
+      },
+      {
+        title: "The Preservationist",
+        author: {
+          firstName: "David",
+          lastName: "Maine"
+        }
+      },
+      {
+        title: "Fallen",
+        author: {
+          firstName: "David",
+          lastName: "Maine"
+        }
+      },
+      {
+        title: "Monster 1959",
+        author: {
+          firstName: "David",
+          lastName: "Maine"
+        }
+      }
+    ];
 
+    var options = {
+      shouldSort: true,
+      threshold: 0.2,
+      location: 0,
+      distance: 100,
+      includeScore: true,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "title",
+        "author.note",
+        "author.tel",
+        "author.firstName",
+        "author.lastName"
+      ]
+    };
+    this.$search("0674585649", datas, options).then(results =>
+      console.log(results)
+    );
+  },
   computed: {
     ...mapState(["phrase"]),
     ...mapGetters(["longeur", "firstLetter"])
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueFuse from "vue-fuse";
 
 Vue.config.productionTip = false;
+Vue.use(VueFuse);
 
 new Vue({
   router,
